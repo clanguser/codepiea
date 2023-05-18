1dadf406f5ee492220ba66a194664e48af73f18c
clanguser <shyaam.s2020@vitstudent.ac.in>
2023-05-18 19:08:08 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+import '@styles/global.css'
+
+export const metadata = {
+    title: "Codepiea",  
+    description: "Discover and Share Codes",
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+        <body>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>
+
+                <main className='app'>
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
+            
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Feed from "@components/Feed"
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> Code Snippets</span>
+      </h1>
+      <p className="desc text-center">
+        Codepiea is an open-source online library to discover and share codes
+      </p>
+
+      <Feed/>
+    </section>
+  )
+}
+
+export default Home
